a23392a0d591f1a0a0a3c2ef9c0417719600d229
Alex Vidyakov <89394067+avidyakov@users.noreply.github.com>
2022-09-18 12:48:13 +0300

    Initial commit

---
+++ job.py
@@ -0,0 +1,6 @@
+class Job:
+    def __init__(self, start_at="", max_working_time=-1, tries=0, dependencies=[]):
+        pass
+
+    def run(self):
+        pass
---
+++ scheduler.py
@@ -0,0 +1,9 @@
+class Scheduler:
+    def __init__(self, pool_size: 10):
+        pass
+
+    def schedule(self, task):
+        pass
+
+    def run(self):
+        pass
